d2f1f6e6fd48ca05b8dcd89ac3f98a1eb3c6a454
SoMin <thals0107@naver.com>
2022-09-23 15:42:30 +0900

    IIFE

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,10 @@
 // @ts-check
-const socket = new WebSocket(`ws://${window.location.host}/chat`);
 
-// socket server가 열리면 socket.send
-socket.addEventListener('open', () => {
-  socket.send('안녕 나 클라이언트');
-});
+// IIFE
+(() => {
+  const socket = new WebSocket(`ws://${window.location.host}/chat`);
+  // socket server가 열리면 socket.send
+  socket.addEventListener('open', () => {
+    socket.send('안녕 나 클라이언트');
+  });
+})();
