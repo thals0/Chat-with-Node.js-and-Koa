7910459f8ccf8778c26de97b1651984e2ceb52c4
SoMin <thals0107@naver.com>
2022-09-23 14:34:13 +0900

    pug 설치 및 실행

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 
 const Koa = require('koa');
 
+const Pug = require('koa-pug');
+const path = require('path');
+
 const app = new Koa();
 const PORT = 4500;
 
+const pug = new Pug({
+  viewPath: path.resolve(__dirname, './views'),
+  app,
+});
+
 app.use(async (ctx, next) => {
-  console.log(ctx.request);
-  console.log(ctx.response);
-  ctx.body = 'hello';
+  // console.log(ctx.request);
+  // console.log(ctx.response);
+  // ctx.body = 'hello, koa world';
+  await ctx.render('chat');
 });
 
 app.listen(PORT, () => {
