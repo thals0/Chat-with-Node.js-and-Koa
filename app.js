84f9e6a86c929a710419f9b1e9c7340c15e99dee
SoMin <thals0107@naver.com>
2022-09-23 17:42:17 +0900

    chatting 기능 구현

--- app.js
+++ app.js
@@ -19,12 +19,46 @@ const pug = new Pug({
   app,
 });
 
+// console.log(app.ws);
+
 // 모든 주소를 ./chat으로 받아줌
 app.ws.use(
   route.all('/chat', (ctx) => {
-    ctx.websocket.send('여긴 서버');
+    // const server = app.ws.server;
+    // 구조분해할당 방식으로 받아옴
+    const { server } = app.ws;
+
+    server?.clients.forEach((client) => {
+      client.send(
+        JSON.stringify({
+          name: '서버',
+          msg: `새로운 유저가 참여 했습니다. 현재 유저 수 ${server.clients.size}`,
+          bg: 'bg-danger',
+          text: 'text-white',
+        })
+      );
+      // client.send('모든 클라이언트에게 데이터를 보냄');
+      // client.send('새로운 유저가 입장하였습니다.');
+    });
+    // ctx.websocket.send('여긴 서버');
     ctx.websocket.on('message', (message) => {
-      console.log(message.toString());
+      // console.log(message.toString());
+      server?.clients.forEach((client) => {
+        client.send(message.toString());
+      });
+    });
+    // 어떤 클라이언트가 close(나갔을)시
+    ctx.websocket.on('close', () => {
+      server?.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            name: '서버',
+            msg: `유저 한명이 나갔습니다. 현재 유저 수 ${server.clients.size}`,
+            bg: 'bg-dark',
+            text: 'text-white',
+          })
+        );
+      });
     });
   })
 );
