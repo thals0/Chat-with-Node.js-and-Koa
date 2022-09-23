13b2824d0bc329d60955f1488fb05ecb3b8bfa80
SoMin <thals0107@naver.com>
2022-09-23 15:39:28 +0900

    클라이언트에서 통신 보내기

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 // @ts-check
 
 const Koa = require('koa');
+const websockify = require('koa-websocket');
+const route = require('koa-route');
+const serve = require('koa-static');
+const mount = require('koa-mount');
 
 const Pug = require('koa-pug');
 const path = require('path');
 
-const app = new Koa();
+const app = websockify(new Koa());
 const PORT = 4500;
+
+app.use(mount('/public', serve('public')));
 
 const pug = new Pug({
   viewPath: path.resolve(__dirname, './views'),
   app,
 });
+
+// 모든 주소를 ./chat으로 받아줌
+app.ws.use(
+  route.all('/chat', (ctx) => {
+    ctx.websocket.send('여긴 서버');
+    ctx.websocket.on('message', (message) => {
+      console.log(message.toString());
+    });
+  })
+);
 
 app.use(async (ctx, next) => {
   // console.log(ctx.request);
